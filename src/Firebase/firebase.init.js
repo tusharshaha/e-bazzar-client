9e9d1f1d612acf40af01e2f1db5fe09721c1f438
tusharshaha <tusharshaha128@gmail.com>
2022-01-02 10:07:28 +0600

    firebase initialize

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,4 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from './firebase.config';
+const initializeAuthentication = () => initializeApp(firebaseConfig);
+export default initializeAuthentication;
